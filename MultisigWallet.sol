249ff9c476df16e76572c4435234279e6fd0c9d6
jmbalogog <jmbtob@gmail.com>
2021-03-15 19:16:57 +0100

    first commit in project

---
+++ MultisigWallet.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.6.0;
+
+contract Wallet {
+	address [] public approvers;
+	uint public quorum;
+
+	constructor(address memory _approvers, uint _quorum) public {
+		approvers = _approvers;
+		quorum = _quorum;
+	}
+
+}
+
+
