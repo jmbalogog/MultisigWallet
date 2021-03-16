4366ad5ca87d7bdb3b35b780598750a573d81040
jmbalogog <jmbtob@gmail.com>
2021-03-16 13:54:22 +0100

    final smart contract

--- MultisigWallet.sol
+++ MultisigWallet.sol
@@ -40,3 +40,35 @@ contract Wallet {
 		));
 	}
 
+	function approveTransfer(uint id) external onlyApprover(){
+		require(transfers[id].sent == false, 'transfer has already been sent');
+		require(approvals[msg.sender][id]== false, 'cannot approve transfer twice');
+
+		approvals[msg.sender][id] = true;
+		transfers[id].approvals++;
+
+		if(transfers[id].approvals >= quorum) {
+			transfers[id].sent = true;
+			address payable to = transfers[id].to;
+			uint amount = transfers[id].amount;
+			to.transfer(amount);
+
+		}
+
+		
+	}
+
+	receive() external payable {}  // fallback function to receive ether in the smart contract
+
+	modifier onlyApprover() {
+		bool allowed = false;
+		for(uint i = 0; i < approvers.length; i++) {
+			if(approvers[i] == msg.sender) {
+				allowed = true;
+			}
+		}
+		require(allowed == true, 'only approver allowed');
+		_;
+	}
+
+}
